e2c7ae1833869c5ac1f3b3ed4223cd2c0de96a96
Kai <158809801+kajwich@users.noreply.github.com>
2025-03-16 15:19:28 +0100

    improved comments

--- EnEx_API.py
+++ EnEx_API.py
@@ -1,18 +1,17 @@
-# v 0.1.1
-# 2025-03-13
+# v 0.1.2
+# 2025-03-16
 
-import requests  # Importiert das requests-Modul für HTTP-Anfragen
-import os  # Importiert das os-Modul für Betriebssystem-Funktionen
-from dotenv import load_dotenv  # Importiert load_dotenv, um Umgebungsvariablen aus einer .env-Datei zu laden
+import requests
+import os
+from dotenv import load_dotenv
 
-load_dotenv()  # Lädt Umgebungsvariablen aus einer .env-Datei
+load_dotenv()
 
-api_token = os.getenv("WIKI_ACCESS_TOKEN")  # Holt den API-Token aus den Umgebungsvariablen
+api_token = os.getenv("WIKI_ACCESS_TOKEN")
 
-# Definiert die Header für die HTTP-Anfrage
 headers = {
-        'Authorization': 'Bearer ' + api_token,  # Setzt den Authorization-Header
-        'User-Agent': 'EnEx (kajwich@icloud.com)'  # Setzt den User-Agent-Header
+        'Authorization': 'Bearer ' + api_token,
+        'User-Agent': 'EnEx (kajwich@icloud.com)'
         }
 
 class Wikipedia:
@@ -22,8 +21,8 @@ class Wikipedia:
 
         :param name: Der Name der Wikipedia-Seite.
         """
-        self.name = name  # Setzt den Namen der Wikipedia-Seite
-        self.url = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + self.name + '/html'  # Setzt die URL für die API-Anfrage
+        self.name = name
+        self.url = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + self.name + '/html'
 
     def fetch(self):
         """
@@ -31,5 +30,5 @@ class Wikipedia:
 
         :return: Der Text der Antwort der API-Anfrage.
         """
-        response = requests.get(self.url, headers=headers)  # Führt eine GET-Anfrage an die API durch
-        return response.text  # Gibt den Text der Antwort zurück
+        response = requests.get(self.url, headers=headers)
+        return response.text
