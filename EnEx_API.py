a3d59fdf64a70a84a5d4b6e5048cf9c94927d114
Kai <158809801+kajwich@users.noreply.github.com>
2025-03-18 15:57:54 +0100

    ExEx_API.py v 0.2 wikipedia.py v 0.1

--- EnEx_API.py
+++ EnEx_API.py
@@ -1,9 +1,10 @@
-# v 0.1.2
-# 2025-03-16
+# v 0.1.4
 
+import json
 import requests
 import os
 from dotenv import load_dotenv
+from Wikipedia import Wikipedia
 
 load_dotenv()
 
@@ -14,21 +15,51 @@ headers = {
         'User-Agent': 'EnEx (kajwich@icloud.com)'
         }
 
-class Wikipedia:
-    def __init__(self, name: str):
-        """
-        Initialisiert eine neue Instanz der Wikipedia-Klasse.
+def get_wiki_page(name: str):
+    """
+    Holt die HTML-Datei einer Wikipedia-Seite mittels einer API-Anfrage.
 
-        :param name: Der Name der Wikipedia-Seite.
-        """
-        self.name = name
-        self.url = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + self.name + '/html'
+    :param name: Der Name der Wikipedia-Seite.
+    :return: Der Text der Antwort der API-Anfrage.
+    """
+    url = 'https://api.wikimedia.org/core/v1/wikipedia/de/page/' + name + '/html'
+    response = requests.get(url, headers=headers)
+    wiki = Wikipedia(name)
+    wiki.inhalt = response.text
+    return wiki
 
-    def fetch(self):
-        """
-        Holt die HTML-Datei der Wikipedia-Seite mittels einer API-Anfrage.
+def get_inbound_links(name: str):
+    """
+    Holt die eingehenden Links einer Wikipedia-Seite mittels einer API-Anfrage.
 
-        :return: Der Text der Antwort der API-Anfrage.
-        """
-        response = requests.get(self.url, headers=headers)
-        return response.text
+    :param name: Der Name der Wikipedia-Seite.
+    :return: Die Antwort der API-Anfrage als JSON-Objekt
+    """
+    url = 'https://de.wikipedia.org/w/api.php?action=query&format=json&uselang=de&list=backlinks&formatversion=2&bltitle=' + name
+    response = requests.get(url)
+    return response.json()
+
+def get_outbound_links(name: str):
+    """
+    Holt die ausgehenden Links einer Wikipedia-Seite mittels einer API-Anfrage.
+
+    :param name: Der Name der Wikipedia-Seite.
+    :return: Die Antwort der API-Anfrage als JSON-Objekt
+    """
+    url = 'https://de.wikipedia.org/w/api.php?action=query&prop=links&titles=' + name + '&pllimit=max&format=json'
+    response = requests.get(url)
+    return response.json()
+
+def get_wiki_links(name: str):
+    """
+    Holt die eingehenden und ausgehenden Links einer Wikipedia-Seite mittels einer API-Anfrage.
+
+    :param name: Der Name der Wikipedia-Seite.
+    :return: Die eingehenden und ausgehenden Links als JSON-Objekt
+    """
+    inbound_links = json.loads(get_inbound_links(name))
+    outbound_links = json.loads(get_outbound_links(name))
+    wiki = Wikipedia(name)
+    wiki.inbound_links = inbound_links
+    wiki.outbound_links = outbound_links
+    return wiki
--- wikipedia.py
+++ wikipedia.py
@@ -1,39 +1,20 @@
 # v 0.1
 
-import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-api_token = os.getenv("WIKI_ACCESS_TOKEN")
-
-headers = {
-        'Authorization': 'Bearer ' + api_token,
-        'User-Agent': 'EnEx (kajwich@icloud.com)'
-        }
-
-
-# Das Wikipedia Objekt zum Speichern von der Wikipedialinks
-
 class Wikipedia:
-
-    def __init__(self, titel):
-        self.titel = titel
-        self.link = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + self.titel + '/html'
-
-    
-
-# link -> done
-# inhalt
-# ausgehendeLinks 
-# eingehendeLinks
-
-def fetch(site: Wikipedia):
         """
-        Holt die HTML-Datei der Wikipedia-Seite mittels einer API-Anfrage.
+        Ein Objekt, das Informationen über eine Wikipedia-Seite speichert.
 
-        :return: Der Text der Antwort der API-Anfrage.
+        Attribute:
+        titel (str): Der Titel der Wikipedia-Seite.
+        link (str): Der API-Link zur HTML-Version der Seite.
+        inhalt (str): Der Inhalt der Seite.
+        ausgehendeLinks (list): Eine Liste von ausgehenden Links auf der Seite.
+        eingehendeLinks (list): Eine Liste von eingehenden Links zur Seite.
         """
-        response = requests.get(site.link, headers=headers)
-        return response.text
+
+        def __init__(self, titel):
+                self.titel = titel
+                self.link = 'https://api.wikimedia.org/core/v1/wikipedia/en/page/' + self.name + '/html'
+                self.inhalt = ""
+                self.ausgehendeLinks = []
+                self.eingehendeLinks = []
